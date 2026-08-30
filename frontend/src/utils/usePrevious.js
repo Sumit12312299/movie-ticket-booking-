ae28c500bfb8809eb5dc2485f18f8e3215ee10d0
sumit12312299 <sumitkr4364@gmail.com>
2026-08-30 23:47:28 +0530

    feat(hooks): add usePrevious hook for tracking prior render values

---
+++ frontend/src/utils/usePrevious.js
@@ -0,0 +1,11 @@
+import { useEffect, useRef } from 'react';
+
+const usePrevious = (value) => {
+  const ref = useRef(undefined);
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+};
+
+export default usePrevious;
